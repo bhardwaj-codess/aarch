a4017459da99773108d2631007617afc4b911dcb
Abhishek Gaur <abhibhardwaj@Abhis-MacBook-Air.local>
2025-09-09 23:44:34 +0530

    Your commit message

--- src/utils/sendOtp.js
+++ src/utils/sendOtp.js
@@ -1,21 +1,23 @@
-// utils/sendOtp.js
-import twilio from 'twilio';
+// src/utils/sendOtp.js
+const twilio = require('twilio');
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = twilio(accountSid, authToken);
 
-export async function sendOtpSms(to, otp) {
+async function sendOtpSms(to, otp) {
   try {
     const message = await client.messages.create({
       body: `Your OTP is: ${otp}`,
-      from: process.env.TWILIO_PHONE_NUMBER, // Your Twilio number
+      from: process.env.TWILIO_PHONE_NUMBER, // Twilio number
       to: to
     });
-    console.log("Message sent:", message.sid);
+    console.log('Message sent:', message.sid);
     return true;
   } catch (err) {
-    console.error("Error sending OTP via SMS:", err);
+    console.error('Error sending OTP via SMS:', err);
     return false;
   }
 }
+
+module.exports = { sendOtpSms };
