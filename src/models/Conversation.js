dbe99f303a72f3ac23d2c7b7a7fed5797f2e234a
Abhishek Gaur <abhibhardwaj@Abhis-MacBook-Air.local>
2025-09-22 21:31:15 +0530

    updated

--- src/models/Conversation.js
+++ src/models/Conversation.js
@@ -1,19 +1,3 @@
-// // models/Conversation.js
-// const conversationSchema = new Schema({
-//   participants: [{ type: ObjectId, ref: 'User' }],   // always 2 people
-//   lastMessage:  { text: String, sender: ObjectId, createdAt: Date },
-//   updatedAt:    { type: Date, default: Date.now }
-// });
-
-// // models/Message.js
-// const messageSchema = new Schema({
-//   conversationId: { type: ObjectId, ref: 'Conversation', index: true },
-//   senderId:       { type: ObjectId, ref: 'User' },
-//   text:           { type: String, maxlength: 1000 },
-//   read:           { type: Boolean, default: false }
-// }, { timestamps: true });
-
-
 // models/Conversation.js
 const { Schema, model, Types } = require('mongoose');
 
