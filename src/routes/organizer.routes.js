b24f6e530317ac478f0b63fb60d06a6485e28072
Abhishek Gaur <abhibhardwaj@Abhis-MacBook-Air.local>
2025-09-25 16:22:00 +0530

    updated routes

--- src/routes/organizer.routes.js
+++ src/routes/organizer.routes.js
@@ -3,7 +3,7 @@ const ctrl   = require('../controllers/organizer.controller');
 const auth = require('../middlewares/auth'); 
 
 router.post('/create-organizer',     auth, ctrl.createOrganizer);
-router.put('/edit-organizer',    auth, ctrl.updateOrganizer);
+router.post('/edit-organizer',    auth, ctrl.updateOrganizer);
 router.get('/me',    auth, ctrl.getMyOrganizer);
 router.get('/organizers', ctrl.listOrganizers);
 router.get('/:id',            ctrl.getOrganizerById);
